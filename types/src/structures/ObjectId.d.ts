5c1cc9766d6a4b1a002fab322c842311b743d7c0
Nayan Bhut <nayanbhut03@gmail.com>
2021-08-18 02:03:26 -0400

    Few Changes to types

--- types/src/structures/ObjectId.d.ts
+++ types/src/structures/ObjectId.d.ts
@@ -1,77 +1,5 @@
 /** A class representation of the BSON ObjectId type. */
 export declare class ObjectId {
-<<<<<<< HEAD:types/src/structures/ObjectId.d.ts
-    /**
-     * Create ObjectId
-     * @param {string} [id] Hexadecimal string of ObjectId
-     */
-    constructor(id?: string);
-    /**
-     * ObjectId's index
-     * @type {number}
-     * @static
-     */
-    static index: number;
-    /**
-     * 5 byte random number for the current process
-     * @type {Uint8Array}
-     * @static
-     */
-    static processRandom5Bytes: Uint8Array;
-    /**
-     * ObjectId string pattern
-     * @type {RegExp}
-     * @static
-     */
-    static OBJECTID_PATTERN: RegExp;
-    /**
-     * ObjectId as buffer
-     * @type {Buffer}
-     * @private
-     */
-    private id;
-    /**
-     * ObjectId in hexadecimal string format
-     * @type {string}
-     * @readonly
-     */
-    get str(): string;
-    /**
-     * The timestamp portion of the ObjectId as a Date.
-     * @returns {Date}
-     */
-    getTimestamp(): Date;
-    /**
-     * The string representation of the ObjectId. This string value has the format of ObjectId(...).
-     * @returns {string}
-     */
-    toString(): string;
-    /**
-     * ObjectId id value in hexadecimal string.
-     * @returns {string}
-     */
-    valueOf(): string;
-    /**
-     * Returns the id if it's valid otherwise it throws an error.
-     * @param {string} id Hexadecimal string of ObjectId class
-     * @returns {void}
-     * @throws {TypeError} Throws an error if id is not a valid Object Id.
-     * @private
-     */
-    static validateId(id: unknown): void;
-    /**
-     * Increments ObjectId's index.
-     * @returns {number}
-     * @private
-     */
-    private incrementIndex;
-    /**
-     * Generates a 12 byte unique ObjectId as buffer.
-     * @returns {Buffer}
-     * @private
-     */
-    private generate;
-=======
   /**
    * Create ObjectId
    * @param {string} [id] Hexadecimal string of ObjectId
@@ -142,5 +70,4 @@ export declare class ObjectId {
    * @private
    */
   private generate;
->>>>>>> a4ae36663598168dfb21c8cf231a41d2f1cd74ac:types/structures/ObjectId.d.ts
 }
--- types/src/tools/types.d.ts
+++ types/src/tools/types.d.ts
@@ -1,27 +1,17 @@
-<<<<<<< HEAD:types/src/tools/types.d.ts
-export interface UnknownObject extends Object {
-    [key: string]: any;
-=======
 export interface UnknownObject {
   [key: string]: any;
->>>>>>> a4ae36663598168dfb21c8cf231a41d2f1cd74ac:types/tools/types.d.ts
 }
 export interface ProjectionObject extends Document {
-    [key: string]: 0 | 1 | ProjectionObject;
+  [key: string]: 0 | 1 | ProjectionObject;
 }
 export declare type JSONArray = Array<string | number | JSONObject | JSONArray | boolean | null>;
 export interface JSONObject {
   [key: string]: string | number | JSONObject | JSONArray | boolean | null;
 }
-export interface Document extends JSONObject {
-}
+export interface Document extends JSONObject {}
 export declare type Query<D extends Document> = Partial<D>;
 export interface IndexSpecification {
-<<<<<<< HEAD:types/src/tools/types.d.ts
-    [key: string]: 0 | IndexSpecification;
-=======
   [key: string]: 0;
->>>>>>> a4ae36663598168dfb21c8cf231a41d2f1cd74ac:types/tools/types.d.ts
 }
 export interface IndexObject extends JSONObject {
   indexSpecification: IndexSpecification;
@@ -31,22 +21,12 @@ export interface IndexesObject extends JSONObject {
   indexes: IndexObject[];
 }
 export interface IndexedDocumentObject extends JSONObject {
-<<<<<<< HEAD:types/src/tools/types.d.ts
-    indexedDocument: JSONObject;
-    _id: string;
-}
-export interface IndexedDocumentsObject extends JSONObject {
-    indexSpecification: IndexSpecification;
-    indexProjection: ProjectionObject;
-    indexedDocuments: IndexedDocumentObject[];
-=======
   indexQuery: JSONObject;
   _id: string;
 }
 export interface IndexedDocumentsObject extends JSONObject {
   indexedDocuments: IndexedDocumentObject[];
->>>>>>> a4ae36663598168dfb21c8cf231a41d2f1cd74ac:types/tools/types.d.ts
 }
 export declare type Projection<T extends Document> = {
-    [Key in keyof T]?: 0 | 1;
+  [Key in keyof T]?: 0 | 1;
 };
